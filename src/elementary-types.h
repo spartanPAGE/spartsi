dbb32f99544ab2bdb97fbf470130df347ef96a7e
Patryk Wertka <spartanpageagain@gmail.com>
2016-11-14 00:18:15 +0100

    {attributes, nodes}_map now keeps variant of regular and ref versions of elements

--- src/elementary-types.h
+++ src/elementary-types.h
@@ -3,6 +3,7 @@
 #include <string>
 #include "utensils/history_keeping_map.h"
 #include "boost/unordered_map.hpp"
+#include "boost/variant.hpp"
 
 namespace spartsi {
     using name = std::string;
@@ -10,14 +11,26 @@ namespace spartsi {
     using map = history_keeping_map<boost::unordered_map<Key, Value>>;
 
     struct attribute {
+        std::string value, comment;
+    };
+    struct ref_attribute {
         std::string value, comment, ref_comment;
     };
-    using attributes_map = map<name, attribute>;
+    using attribute_variant = boost::variant<attribute, ref_attribute>;
+    using attributes_map = map<name, attribute_variant>;
 
     struct node;
-    using nodes_map = map<name, node>;
+    struct ref_node;
+    using nodes_variant = boost::variant<node, ref_node>;
+    using nodes_map = map<name, nodes_variant>;
 
     struct node {
+        std::string comment;
+        nodes_map children;
+        attributes_map attributes;
+    };
+
+    struct ref_node {
         std::string comment, ref_comment;
         nodes_map children;
         attributes_map attributes;
--- test/elementary-types.cpp
+++ test/elementary-types.cpp
@@ -6,32 +6,45 @@ TEST_CASE("everything compiles well", "[spartsi::elementary_types*") {
         "example tree",
         "with some example data",
         spartsi::nodes_map {
-            { "node", spartsi::node {
+            { "node", spartsi::ref_node {
                 "comment", "ref comment"
             }}
         },
         spartsi::attributes_map {
             { "attr", spartsi::attribute {
-                "value", "comment", "ref comment"
+                "value", "comment"
             }},
 
-            { "attr 2", spartsi::attribute {
-                "value 2", "comment 2", "ref comment 2"
+            { "attr 2", spartsi::ref_attribute {
+                "value 2", "comment 2", "ref comment"
             }}
         }
     };
 
-    REQUIRE(subject.name == "example tree");
-    REQUIRE(subject.comment == "with some example data");
-    REQUIRE(subject.children.count("node"));
-    REQUIRE(subject.children.at("node").comment == "comment");
-    REQUIRE(subject.children.at("node").ref_comment == "ref comment");
-    REQUIRE(subject.attributes.count("attr"));
-    REQUIRE(subject.attributes.at("attr").value == "value");
-    REQUIRE(subject.attributes.at("attr").comment == "comment");
-    REQUIRE(subject.attributes.at("attr").ref_comment == "ref comment");
-    REQUIRE(subject.attributes.count("attr 2"));
-    REQUIRE(subject.attributes.at("attr 2").value == "value 2");
-    REQUIRE(subject.attributes.at("attr 2").comment == "comment 2");
-    REQUIRE(subject.attributes.at("attr 2").ref_comment == "ref comment 2");
+    SECTION("validate root data") {
+        REQUIRE(subject.name == "example tree");
+        REQUIRE(subject.comment == "with some example data");
+        REQUIRE(subject.children.count("node"));
+        REQUIRE(subject.attributes.count("attr"));
+        REQUIRE(subject.attributes.count("attr 2"));
+    }
+
+    SECTION("validate node") {
+        auto &node = boost::get<spartsi::ref_node>(subject.children.at("node"));
+        REQUIRE(node.comment == "comment");
+        REQUIRE(node.ref_comment == "ref comment");
+    }
+
+    SECTION("validate attr") {
+        auto &attr = boost::get<spartsi::attribute>(subject.attributes.at("attr"));
+        REQUIRE(attr.value == "value");
+        REQUIRE(attr.comment == "comment");
+    }
+
+    SECTION("validate attr2") {
+        auto &attr2 = boost::get<spartsi::ref_attribute>(subject.attributes.at("attr 2"));
+        REQUIRE(attr2.value == "value 2");
+        REQUIRE(attr2.comment == "comment 2");
+        REQUIRE(attr2.ref_comment == "ref comment");
+    }
 }
